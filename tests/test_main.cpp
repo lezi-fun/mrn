48ccb8a0754882edad56924e182af77d51457761
lezi-fun <roy20130108@163.com>
2025-11-30 14:37:03 +0800

    Create test_main.cpp

---
+++ tests/test_main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "core/version.h"
+
+int main() {
+    std::cout << "MRN Version: " << MRN_VERSION_STRING << std::endl;
+    std::cout << "Basic test passed!" << std::endl;
+    return 0;
+}
